70829a8fbb60f4e3f3a800bbd4b0d8332960cca9
Daniel Peach <dpeaches96@gmail.com>
2019-10-04 17:37:40 -0400

    stuff

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -3,14 +3,18 @@
 
 int main() {
 
-  int dpi = 2;
+  int dpi = 1;
   int w = 500 * dpi;
   int h = 500 * dpi;
 
   int r = 75 * dpi;
-  int v = 1;
+  int v = 500 * dpi;
 
-  sf::RenderWindow screen(sf::VideoMode(w, h), "Primer");
+  sf::RenderWindow screen(sf::VideoMode(w, h), "Primer - SFML");
+
+  sf::Clock clock;
+  float elapsedTime;
+  float fps = 1 / 60.f;
 
   sf::CircleShape circle(r);
   circle.setFillColor(sf::Color(0, 0, 255));
@@ -18,6 +22,8 @@ int main() {
   circle.setPosition(screen.getSize().x / 2.f, screen.getSize().y / 2.f);
 
   while (screen.isOpen()) {
+    elapsedTime += clock.restart().asSeconds();
+
     int x = 0;
     int y = 0;
 
@@ -28,28 +34,41 @@ int main() {
       }
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-      if(circle.getPosition().y - v > r) {
-        y -= v;
-      }
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-      if(circle.getPosition().y + v < h - r) {
-        y += v;
-      }
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-      if(circle.getPosition().x - v > r) {
-        x -= v;
-      }
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-      if(circle.getPosition().x + v < w - r) {
-        x += v;
-      }
-    }
+    while (elapsedTime >= fps) {
+      elapsedTime -= fps;
 
-    circle.move(x, y);
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        if(circle.getPosition().y - (v * fps) > r) {
+          y -= (v * fps);
+        } else {
+          y -= circle.getPosition().y - r;
+        }
+      }
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        if(circle.getPosition().y + (v * fps) < h - r) {
+          y += (v * fps);
+        } else {
+          y += h - (circle.getPosition().y + r);
+        }
+      }
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        if(circle.getPosition().x - (v * fps) > r) {
+          x -= (v * fps);
+        } else {
+          x -= circle.getPosition().x - r;
+        }
+      }
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        if(circle.getPosition().x + (v * fps) < w - r) {
+          x += (v * fps);
+        } else {
+          x += w - (circle.getPosition().x + r);
+        }
+      }
+
+      circle.move(x, y);
+
+    }
 
     screen.clear(sf::Color(255, 255, 255));
     screen.draw(circle);
